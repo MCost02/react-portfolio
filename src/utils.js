181860a85ac98a90b77f18c93cedc0995eee2b92
Marx Costanzo Jr <marxcostanzo@yahoo.com>
2024-06-30 23:00:38 -0400

    Added images

---
+++ src/utils.js
@@ -0,0 +1,4 @@
+/* Helps dynamically import images to site components by constructing absolute paths to each image */
+export const getImageUrl = (path) => {
+    return new URL('assets/${path}', import.meta.url).href;
+};
