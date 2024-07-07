2904b1d0d74763c5fc26d480221f74dbf9a03620
Marx Costanzo Jr <marxcostanzo@yahoo.com>
2024-07-07 13:14:53 -0400

    Content for experience section is now dynamically loaded and displayed

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Experience.module.css";
+import experience from "../../data/experience.json";
 import { getImageUrl } from "../../utils";
 
 export const Experience = () => {
@@ -7,17 +8,28 @@ export const Experience = () => {
     <section className={styles.container}>
       <h2 className={styles.title}>Experience</h2>
       <div className={styles.content}>
+        {/* Data is loaded in from experience.json */}
         <ul>
-          <li>
-            <img src={getImageUrl(".png")} alt="Company Logo" />
-            <div>
-              <h3>Job Title, Company</h3>
-              <p>Start Date - End Date</p>
-              <ul>
-                <li>details</li>
-              </ul>
-            </div>
-          </li>
+          {experience.map((experienceItem, id) => {
+            return (
+              <li key={id}>
+                <img
+                  src={getImageUrl(experienceItem.logo)}
+                  alt={`${experienceItem.company} Logo`}
+                />
+                <div>
+                  <h3>{`${experienceItem.role}, ${experienceItem.company}`}</h3>
+                  <p>{`${experienceItem.startDate} - ${experienceItem.endDate}`}</p>
+                  <ul>
+                    {/* Lists all the details associated with each role */}
+                    {experienceItem.details.map((details, id) => {
+                      return <li key={id}>{details}</li>;
+                    })}
+                  </ul>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </section>
